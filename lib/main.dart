68ce0fbfacf9addab029d9867d8af1b2a9f60e7d
Shinnn13 <sultanilham8@gmail.com>
2022-06-14 13:15:12 +0700

    menambahkan line untuk mengilangkan debug

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Moon Car',
       theme: ThemeData(
         backgroundColor: const Color.fromARGB(255, 241, 242, 246),
